c45971428784617b67074c5a3df067f5dba6e24f
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2023-11-17 13:54:32 +0700

    Supabase setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,35 @@
+<!-- src/routes/+layout.svelte -->
 <script>
 	import '../app.postcss';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	export let data;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => subscription.unsubscribe();
+	});
+
+	async function logout() {
+		await supabase.auth.signOut();
+	}
 </script>
 
-<slot />
+<svelte:head>
+	<title>YumRank</title>
+</svelte:head>
+
+<div class="container" style="padding: 50px 0 100px 0">
+	<slot />
+</div>
