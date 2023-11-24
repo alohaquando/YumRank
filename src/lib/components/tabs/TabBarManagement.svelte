2e8568d3cf6e41ff047667ee0b477784dea81050
Quân Đỗ <aloha.quando@gmail.com>
2023-11-25 03:10:17 +0700

    Added TabBar

---
+++ src/lib/components/tabs/TabBarManagement.svelte
@@ -0,0 +1,22 @@
+<script>
+import LinkChip from '$lib/components/chips/LinkChip.svelte';
+import TabBar from '$lib/components/tabs/TabBar.svelte';
+</script>
+
+<TabBar>
+	<LinkChip width="fit" href="/">
+		Today
+	</LinkChip>
+	<LinkChip width="fit" href="/">
+		Posts
+	</LinkChip>
+	<LinkChip width="fit" href="/">
+		Discounts
+	</LinkChip>
+	<LinkChip width="fit" href="/">
+		All check-ins
+	</LinkChip>
+	<LinkChip width="fit" href="/">
+		Settings
+	</LinkChip>
+</TabBar>
