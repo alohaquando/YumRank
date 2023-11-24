49e39ebe8c37fd3d4f82789d9ead19bb9291103b
Quân Đỗ <aloha.quando@gmail.com>
2023-11-25 02:36:58 +0700

    Added Chip components

---
+++ src/lib/components/chips/ActionChip.svelte
@@ -0,0 +1,63 @@
+<!--suppress JSUnreachableSwitchBranches -->
+<script lang="ts">
+	export let type: 'button' | 'submit' | 'reset' | null | undefined = 'button';
+	export let size: 'md' | 'sm' = 'md';
+	export let design: 'filled' | 'outlined' | 'tonal' | 'text' = 'outlined';
+	export let disabled: boolean = false;
+	export let width: "fit" | "full" = "fit";
+	let customClasses: string = '';
+	export { customClasses as class };
+
+	let sizeClasses: string;
+	switch (size) {
+		case 'md': {
+			sizeClasses = 'px-4 h-8';
+			break;
+		}
+	}
+
+	let widthClasses: string;
+	switch (width) {
+		case 'fit': {
+			widthClasses = 'w-fit';
+			break;
+		}
+		case 'full': {
+			widthClasses = 'w-full';
+			break;
+		}
+	}
+
+
+	let designClasses: string;
+	switch (design) {
+		case 'filled': {
+			designClasses = 'bg-red-500 text-white hover:bg-red-600';
+			break;
+		}
+		case 'outlined': {
+			designClasses = 'outline outline-1 outline-gray-300 hover:bg-red-50';
+			break;
+		}
+		case 'tonal': {
+			designClasses = 'bg-red-100 text-red-500 hover:bg-red-500 hover:text-white';
+			break;
+		}
+		case 'text': {
+			designClasses = 'hover:bg-red-50';
+			break;
+		}
+	}
+</script>
+
+<button on:click {disabled} {type}
+				class="flex justify-center items-center rounded-full transition space-x-2 {sizeClasses} {designClasses} {widthClasses} {customClasses}">
+	{#if $$slots.icon}
+		<div>
+			<slot name="icon" />
+		</div>
+	{/if}
+	<label class="font-sans trim-both font-medium">
+		<slot />
+	</label>
+</button>
---
+++ src/lib/components/chips/InfoChip.svelte
@@ -0,0 +1,66 @@
+<!--suppress JSUnreachableSwitchBranches -->
+<script lang="ts">
+	// eslint-disable-next-line @typescript-eslint/prefer-as-const
+	export let size: 'md' | 'sm' = 'md';
+	export let design: 'filled' | 'outlined' | 'tonal' | 'text' | 'subtle' = 'subtle';
+	export let width: "fit" | "full" = "fit";
+	let customClasses: string = '';
+	export { customClasses as class };
+
+	let sizeClasses: string;
+	switch (size) {
+		case 'md': {
+			sizeClasses = 'px-4 h-8';
+			break;
+		}
+	}
+
+	let widthClasses: string;
+	switch (width) {
+		case 'fit': {
+			widthClasses = 'w-fit';
+			break;
+		}
+		case 'full': {
+			widthClasses = 'w-full';
+			break;
+		}
+	}
+
+	let designClasses: string;
+	switch (design) {
+		case 'filled': {
+			designClasses = 'bg-gray-900/50 text-white font-medium';
+			break;
+		}
+		case 'outlined': {
+			designClasses = 'outline outline-1 outline-gray-300 ';
+			break;
+		}
+		case 'tonal': {
+			designClasses = 'bg-red-100 text-red-500 hover:bg-red-500';
+			break;
+		}
+		case 'text': {
+			designClasses = '';
+			break;
+		}
+		case 'subtle': {
+			designClasses = 'text-gray-400';
+			break;
+		}
+	}
+</script>
+
+<div
+				class="flex justify-center items-center rounded-full transition space-x-2 text-sm {sizeClasses} {designClasses} {widthClasses} {customClasses}">
+	{#if $$slots.icon}
+		<div>
+			<slot name="icon" />
+		</div>
+	{/if}
+	<p class="font-sans trim-both">
+		<slot />
+	</p>
+</div>
+
