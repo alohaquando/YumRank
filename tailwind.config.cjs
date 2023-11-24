0290370e06af965439cbf45b22671ed312bbc1dc
Quân Đỗ <aloha.quando@gmail.com>
2023-11-24 14:36:18 +0700

    Added leading trim for font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,10 +5,18 @@ const config = {
 		fontFamily: {
 			sans: ['Inter', { fontFeatureSettings: '"cv09" , "cv08" , "cv06" , "cv02" , "cv04" , "cv01" , "cv11" , "cv03" , "cv05" , "case"' }, 'system-ui']
 		},
+		fontMetrics: {
+			'Inter': {
+				capHeight: 1490,
+				ascent: 1984,
+				descent: -494,
+				unitsPerEm: 2048
+			}
+		},
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [require('tailwindcss-leading-trim')]
 };
 
 module.exports = config;
