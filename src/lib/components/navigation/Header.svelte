6e23d073fad66782abefd01a25d8ebc39813696a
Quân Đỗ <aloha.quando@gmail.com>
2023-11-24 22:31:18 +0700

    Updated Header component

--- src/lib/components/navigation/Header.svelte
+++ src/lib/components/navigation/Header.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import Logo from '$lib/components/logos/Logo.svelte';
-	export let href: string = "/";
-	export let type: "default" | "management" = "default"
+
+	export let href: string = '/';
+	export let type: 'default' | 'management' = 'default';
+	let customClasses = '';
+	export { customClasses as class };
 </script>
 
-<div class="h-14 border-b border-gray-200 flex">
-
+<div class="h-14 border-b border-gray-200 flex bg-white w-full {customClasses}">
 	<div class="container mx-auto flex">
 		<Logo {href} {type} />
 	</div>
