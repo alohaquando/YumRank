6e78456314ccff9d322beb3af541afe1f09caffe
Quân Đỗ <aloha.quando@gmail.com>
2023-11-24 21:51:43 +0700

    Added Logo, Header component

---
+++ src/lib/components/navigation/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Logo from '$lib/components/logos/Logo.svelte';
+	export let href: string = "/";
+	export let type: "default" | "management" = "default"
+</script>
+
+<div class="h-14 border-b border-gray-200 flex">
+
+	<div class="container mx-auto flex">
+		<Logo {href} {type} />
+	</div>
+</div>
