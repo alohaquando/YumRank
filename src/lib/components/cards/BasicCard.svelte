3e914d22d0636dc893ed7ac97bea0e67f4cdd45d
Quân Đỗ <aloha.quando@gmail.com>
2023-11-24 21:51:29 +0700

    Added BasicCard component

---
+++ src/lib/components/cards/BasicCard.svelte
@@ -0,0 +1,11 @@
+<div class="rounded-[40px] bg-red-50 py-8 px-6 flex-col items-center  justify-center flex">
+	<div class="pb-4">
+		<slot name="start" />
+	</div>
+	<div class="pb-6">
+		<slot name="center" />
+	</div>
+	<div class="self-stretch flex [&>button]:grow">
+		<slot name="end" />
+	</div>
+</div>
