4570a1d70be9b5ec0bdeec63f3a115a1657c7ed2
Quân Đỗ <aloha.quando@gmail.com>
2023-11-25 02:37:26 +0700

    Updated Button component with width fit or full

--- src/lib/components/buttons/Button.svelte
+++ src/lib/components/buttons/Button.svelte
@@ -4,8 +4,9 @@
 
 	export let type: 'button' | 'submit' | 'reset' | null | undefined = 'button';
 	export let size: 'md' | 'lg' = 'md';
-	export let design: 'filled' | 'outlined' = 'filled';
+	export let design: 'filled' | 'outlined' | 'tonal' = 'filled';
 	export let disabled: boolean = false;
+	export let width: "fit" | "full" = "fit";
 	let customClasses: string = '';
 	export { customClasses as class };
 
@@ -21,6 +22,18 @@
 		}
 	}
 
+	let widthClasses: string;
+	switch (width) {
+		case 'fit': {
+			widthClasses = 'w-fit';
+			break;
+		}
+		case 'full': {
+			widthClasses = 'w-full';
+			break;
+		}
+	}
+
 	let designClasses: string;
 	switch (design) {
 		case 'filled': {
@@ -31,12 +44,20 @@
 			designClasses = 'outline outline-1 outline-gray-300 hover:bg-red-50';
 			break;
 		}
+		case 'tonal': {
+			designClasses = 'bg-red-100 text-red-500 hover:bg-red-500 hover:text-white';
+			break;
+		}
 	}
 </script>
 
 <button on:click {disabled} {type}
-				class="flex justify-center items-center rounded-full transition space-x-2 {sizeClasses} {designClasses} {customClasses}">
-	<slot name="icon" />
+				class="flex justify-center items-center rounded-full transition space-x-2 {sizeClasses} {designClasses} {widthClasses} {customClasses}">
+	{#if $$slots.icon}
+		<div>
+			<slot name="icon" />
+		</div>
+	{/if}
 	<Title>
 		<slot />
 	</Title>
