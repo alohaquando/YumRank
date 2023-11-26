5a0235ac2ad2c903af0775726ebf6012e7eaf981
Quân Đỗ <aloha.quando@gmail.com>
2023-11-27 01:04:31 +0700

    Added RestaurantSwitcher.svelte

---
+++ src/lib/components/navigation/RestaurantSwitcher.svelte
@@ -0,0 +1,14 @@
+<script>
+	import TabBar from '$lib/components/navigation/TabBar.svelte';
+	import RestaurantTitleItem from '$lib/components/building_blocks/RestaurantTitleItem.svelte';
+	// noinspection ES6UnusedImports
+	import Fa from 'svelte-fa';
+	import { faChevronDown } from '@fortawesome/pro-regular-svg-icons';
+</script>
+
+<button class="w-full" on:click type="button">
+	<TabBar class="justify-between ">
+		<RestaurantTitleItem></RestaurantTitleItem>
+		<Fa icon={faChevronDown} />
+	</TabBar>
+</button>
