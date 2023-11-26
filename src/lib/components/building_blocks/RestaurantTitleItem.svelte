23e9e9023a11722266192a286f39fa08772b61e0
Quân Đỗ <aloha.quando@gmail.com>
2023-11-27 00:51:16 +0700

    Added Post and RestaurantTitleItem

---
+++ src/lib/components/building_blocks/RestaurantTitleItem.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Title from '$lib/components/typography/Title.svelte';
+	export let name: string | undefined | null = 'Restaurant Name';
+	export let src: string | undefined | null = ' ';
+
+	let imgEle: HTMLElement;
+	const handleError = () => {
+		imgEle.classList.add('hidden')
+	}
+</script>
+
+<div class="flex flex-row space-x-2 items-center">
+	<div class="w-8 h-8 bg-red-50 rounded-xl overflow-clip">
+		<img bind:this={imgEle} class="w-8 h-8 rounded-xl" alt="Logo of {name}" {src} on:error={handleError} loading="lazy" />
+	</div>
+	<Title>{name}</Title>
+</div>
---
+++ src/lib/components/posts/Post.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import RestaurantTitleItem from '$lib/components/building_blocks/RestaurantTitleItem.svelte';
+	import Body from '$lib/components/typography/Body.svelte';
+
+	export let restaurantSrc: string | undefined | null;
+	export let restaurantName: string | undefined | null;
+	export let restaurantHref: string | undefined | null;
+	export let imageSrc: string | undefined | null;
+	export let content: string | undefined | null;
+	export let date: string | undefined | null;
+
+	let imgEle: HTMLElement;
+	const handleError = () => {
+		imgEle.classList.add('hidden');
+	};
+</script>
+
+<div class="flex-col flex space-y-2">
+	<a href={restaurantHref}>
+		<RestaurantTitleItem name={restaurantName} src={restaurantSrc}></RestaurantTitleItem>
+	</a>
+	<div class="w-full h-[11rem] bg-red-50 rounded-2xl overflow-clip">
+		<img alt="Post" bind:this={imgEle} class="" loading="lazy" on:error={handleError}
+				 src={imageSrc} />
+	</div>
+	<div class="flex-col flex space-y-1">
+		<Body>{content}</Body>
+		<Body class="opacity-50">{date}</Body>
+	</div>
+</div>
