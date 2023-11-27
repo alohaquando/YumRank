1d646293b19d895580432315766dd86e76f74b36
Quân Đỗ <aloha.quando@gmail.com>
2023-11-27 19:54:38 +0700

    Added Divider

---
+++ src/lib/components/layouts/Divider.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let full: boolean = false;
+
+	let widthClasses: string;
+	if (full) {
+		widthClasses = "w-screen -translate-x-6"
+	} else {
+		widthClasses = "w-full"
+	}
+</script>
+
+<div class=" h-px bg-gray-200 {widthClasses}"></div>
