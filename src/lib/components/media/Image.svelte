746d00e1fa2b7f14e51b7dc2d175ba94bdef25fd
Quân Đỗ <aloha.quando@gmail.com>
2023-11-27 18:35:21 +0700

    Added image component

---
+++ src/lib/components/media/Image.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let src: string | undefined | null;
+	export let alt: string | undefined | null;
+	export let eager: boolean = false;
+	let customClasses: string = "";
+	export {customClasses as class};
+
+	let imgEle: HTMLElement;
+	const handleError = () => {
+		imgEle.classList.add('hidden')
+	}
+</script>
+
+<div class="bg-red-50 {customClasses}">
+	<img bind:this={imgEle} class=" {customClasses} object-cover object-center" {alt} {src} on:error={handleError} loading={eager ? 'eager' : 'lazy'} />
+</div>
