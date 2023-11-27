c80a0d0b05f9c16e45ca1fe2ed50a73a89998e94
Quân Đỗ <aloha.quando@gmail.com>
2023-11-27 22:59:23 +0700

    Updated Post

--- src/lib/components/posts/Post.svelte
+++ src/lib/components/posts/Post.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import RestaurantTitleItem from '$lib/components/building_blocks/TitleItem.svelte';
+	import TitleItem from '$lib/components/building_blocks/TitleItem.svelte';
 	import Body from '$lib/components/typography/Body.svelte';
 	import Image from '$lib/components/media/Image.svelte';
 
@@ -17,12 +17,11 @@
 </script>
 
 <div class="flex-col flex space-y-2">
-	<a href={restaurantHref} class="w-fit">
-		<RestaurantTitleItem name={restaurantName} src={restaurantSrc}></RestaurantTitleItem>
-	</a>
+	<TitleItem
+		href={restaurantHref} src={restaurantSrc} title={restaurantName}></TitleItem>
 	<div class="w-full h-[11rem] bg-red-50 rounded-2xl overflow-clip">
 		<Image alt="Post" on:error={handleError}
-				 src={imageSrc} />
+					 src={imageSrc} />
 	</div>
 	<div class="flex-col flex space-y-1">
 		<Body>{content}</Body>
