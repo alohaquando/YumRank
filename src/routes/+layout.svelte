4d1af0d9a39df63b17c89ac9edb38e8b5d9e7c94
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2023-11-30 23:32:13 +0700

    Added template for auth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <!-- src/routes/+layout.svelte -->
-<script>
-	import '../app.postcss';
+<script lang="ts">
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
 
@@ -12,7 +11,7 @@
 	onMount(() => {
 		const {
 			data: { subscription }
-		} = supabase.auth.onAuthStateChange((event, _session) => {
+		} = supabase.auth.onAuthStateChange(async (event, _session) => {
 			if (_session?.expires_at !== session?.expires_at) {
 				invalidate('supabase:auth');
 			}
@@ -20,16 +19,24 @@
 
 		return () => subscription.unsubscribe();
 	});
-
-	async function logout() {
-		await supabase.auth.signOut();
-	}
 </script>
 
 <svelte:head>
 	<title>YumRank</title>
 </svelte:head>
 
-<div class="container" style="padding: 50px 0 100px 0">
-	<slot />
-</div>
+<nav style="border: solid; border-width: 0 0 2px; padding-bottom: 5px;">
+	<a href="/">Home</a>
+	{#if session}
+	  <a href="/app">App</a>
+	  <a href="/admin">Admin</a>
+	  <img style="width: 32px; height: 32px; border-radius: 9999px;" src={session.user.user_metadata.avatar_url} alt="person_avatar">
+	  <form method="POST" action="auth?/signout">
+		<button>Logout</button>
+	  </form>
+	{:else}
+	  <a href='/auth'>Login</a>
+	{/if}
+  </nav>
+  
+  <slot />
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let form
+  </script>
+  
+  <form method="POST" action="?/signin">
+    <input name="email" placeholder="email" type="email" value={form?.data?.email ?? ""}>
+    <input name="password" placeholder="password" type="password">
+    <button style="margin-top: 12px;">Login</button>
+    <button formaction="?/signup" style="margin-top: 12px;">Signup</button>
+  </form>
+  <form method="POST" action="?/signin">
+    <button style="margin-top: 12px;" name="provider" value="github">Login with GitHub</button>
+  </form>
+  
+  {#if form?.message}
+    <p>{form.message}</p>
+  {/if}
+  {#if form?.error}
+    <p>{form.error}</p>
+  {/if}
