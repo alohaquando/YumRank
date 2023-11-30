715c85334975d55e621ea5d8b0824b43cdd869d0
Quân Đỗ <aloha.quando@gmail.com>
2023-11-30 23:34:42 +0700

    Added TitleDropdown.svelte

---
+++ src/lib/components/inputs/TitleDropdown.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import Display from '$lib/components/typography/Display.svelte';
+	// noinspection ES6UnusedImports
+	import Fa from 'svelte-fa';
+	import { faChevronDown } from '@fortawesome/pro-solid-svg-icons';
+	import Title from '$lib/components/typography/Title.svelte';
+
+	export let design: 'display' | 'title' = 'display';
+	export let value: string;
+	export let name: string;
+	export let id: string;
+	type ThemeOption = {
+		value: string;
+		title: string;
+	};
+	type Options<K extends string> = {
+		[key in K]: ThemeOption;
+	};
+	export let options: Options<string> = {};
+
+	let borderClasses: string;
+	let iconClasses: string;
+
+	// noinspection JSUnreachableSwitchBranches
+	switch (design) {
+		case 'display': {
+			borderClasses = 'border-b-2 pb-2 space-x-2';
+			iconClasses = 'text-xl';
+			break;
+		}
+		case 'title': {
+			borderClasses = 'border-b pb-1 space-x-1';
+			iconClasses = 'text-xs';
+			break;
+		}
+	}
+</script>
+
+<div class="relative w-fit transition group pointer-events-none">
+	<select
+		aria-label="Select theme for website"
+		bind:value
+		class="absolute -bottom-2 -left-2 -right-2 -top-2 bg-white opacity-0 dark:bg-zinc-900 pointer-events-auto"
+		{id}
+		{name}
+		on:change>
+		{#each Object.values(options) as option}
+			<option value={option.value}>{option.title}</option>
+		{/each}
+	</select>
+	<div
+		class="flex flex-row border-gray-900 w-fit h-fit group-hover:text-red-500 group-hover:border-red-500 transition items-center {borderClasses}">
+		<svelte:component this={design === "display"? Display : Title}>{value}</svelte:component>
+		<Fa class="{iconClasses}" icon={faChevronDown} />
+	</div>
+</div>
--- src/lib/components/layouts/LargePageTitle.svelte
+++ src/lib/components/layouts/LargePageTitle.svelte
@@ -2,6 +2,9 @@
 	import Display from '$lib/components/typography/Display.svelte';
 </script>
 
-<Display class="pt-6 pb-8">
-	<slot />
-</Display>
+<div class="flex flex-row items-baseline space-x-2 pt-6 pb-8">
+	<Display>
+		<slot />
+	</Display>
+	<slot name="dropdown"/>
+</div>
