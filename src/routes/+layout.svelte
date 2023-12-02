201d0bc288f13e3faaa7ed79894e17e90c9231cb
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2023-12-02 23:18:54 +0700

    Fixed loading image (will refactor later)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,7 +19,6 @@
 
 		return () => subscription.unsubscribe();
 	});
-
 </script>
 
 <svelte:head>
@@ -31,12 +30,19 @@
 	{#if session}
 		<a href="/app">App</a>
 		<a href="/admin">Admin</a>
-		<img
-			style="width: 32px; height: 32px; border-radius: 9999px;"
-			src={session.user.user_metadata.avatar_url}
-			alt="person_avatar"
-		/>
-	
+		{#if session.user.app_metadata.provider == 'email'}
+			<img
+				style="width: 32px; height: 32px; border-radius: 9999px;"
+				src={`https://spkuounwjckbvmdirseo.supabase.co/storage/v1/object/sign/avatars/${session.user.user_metadata.avatar_url}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhdmF0YXJzLzAuNTUxNjAxNDE5ODIwNTE4MS5wbmciLCJpYXQiOjE3MDE1MzMzMjIsImV4cCI6MTcwNjcxNzMyMn0.JIKJqv4_R6Y-LESAkZyCJLfbfGicReDlA-2uJqFS9dQ&t=2023-12-02T16%3A08%3A18.309Z`}
+				alt="person_avatar"
+			/>
+		{:else}
+			<img
+				style="width: 32px; height: 32px; border-radius: 9999px;"
+				src={session.user.user_metadata.avatar_url}
+				alt="person_avatar"
+			/>
+		{/if}
 		<form method="POST" action="auth?/signout">
 			<button>Logout</button>
 		</form>
