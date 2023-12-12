c06093dbf7f73cbb94e569abbfe144a965d5e4c1
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2023-12-12 22:56:46 +0700

    Fixed app routing

--- src/routes/app/+page.server.ts
+++ src/routes/app/+page.server.ts
@@ -4,7 +4,7 @@ export const load = async ({ locals: { getSession } }) => {
   const session = await getSession()
   console.log('session', session?.user)
 
-  if (!session?.user.user_metadata.user_name !! (!session?.user.user_metadata.username)) {
+  if (!session?.user.user_metadata.user_name) {
     return redirect(307, '/create-account');
   } else {
     // Continue to stay at the page
