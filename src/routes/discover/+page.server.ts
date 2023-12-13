bc9273789dcf37d20403851a446f1f513fd57652
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2023-12-13 15:36:49 +0700

    Added view posts

---
+++ src/routes/discover/+page.server.ts
@@ -0,0 +1,14 @@
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ locals: { supabase, getSession } }) => {
+    const { data: posts, error } = await supabase
+      .from('posts')
+      .select(`title, context, restaurants (name)`)
+  
+    if (error) {
+      console.error('Error fetching posts:', error);
+      return;
+    }
+  
+    return { posts };
+  };
