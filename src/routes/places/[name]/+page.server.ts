ad6333f2b204bca9504b4478a06c79b2f09caed1
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2023-12-14 23:52:18 +0700

    Fixed routing

--- src/routes/places/[name]/+page.server.ts
+++ src/routes/places/[name]/+page.server.ts
@@ -1,16 +1,23 @@
 import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals: { supabase, getSession }, params }) => {
-
+  const session = await getSession()
   const { data: restaurant, error } = await supabase
   .from('restaurants')
   .select('*')
   .eq('name', params.name.replace('-', ' '));
 
+  let resownern = (restaurant && restaurant[0])?.owner_id as string;
+
+  if (resownern == session?.user.id) { 
+    throw redirect(303, `/management/places/${params.name}`)
+  }
+  
   if (error) {
     console.error(error);
     return;
   }
-  
+
+
   return { restaurant };
 };
