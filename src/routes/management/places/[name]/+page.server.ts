151b65ef3af9459c053950f7b6316f300944f754
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2023-12-14 12:22:09 +0700

    Added routing template

---
+++ src/routes/management/places/[name]/+page.server.ts
@@ -0,0 +1,1 @@
+// create update and delete for restaurant
---
+++ src/routes/places/[name]/create-review/+page.server.ts
@@ -0,0 +1,10 @@
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ locals: { supabase, getSession }, params }) => {
+  const session = await getSession()
+
+  if (!session) {
+    return redirect(301, '/sign-in');
+  }
+
+};
