4ba9984eb29034849fb347daf68f3bfd0d93ffd8
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2023-12-15 17:49:13 +0700

    Fixed some routing (havent refactor)

--- src/routes/places/[name]/+page.svelte
+++ src/routes/places/[name]/+page.svelte
@@ -1,5 +1,82 @@
-<script>
+<script lang="ts">
 	import Button from '$lib/components/buttons/Button.svelte';
+	let webSocketEstablished = false;
+	let ws: WebSocket | null = null;
+	let log: string[] = [];
+
+	export let data;
+
+	const logEvent = (str: string) => {
+		log = [...log, str];
+	};
+
+	export const establishWebSocket = () => {
+		if (webSocketEstablished) return;
+		const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+		ws = new WebSocket(`${protocol}//${window.location.host}/websocket`);
+		
+		ws.addEventListener('open', (event) => {
+			webSocketEstablished = true;
+			console.log('[websocket] connection open', event);
+			logEvent('[websocket] connection open');
+
+		});
+		ws.addEventListener('close', (event) => {
+			console.log('[websocket] connection closed', event);
+			logEvent('[websocket] connection closed');
+		});
+		ws.addEventListener('message', (event) => {
+			console.log('[websocket] message received', event);
+			logEvent(`[websocket] message received: ${event.data}`);
+		});
+	};
+
+	export const requestData = async (placeName: String) => {
+		const res = await fetch(`/places/${placeName}`);
+		const data = await res.json();
+		console.log('Data from GET endpoint', data);
+		console.log(data)
+		logEvent(`[GET] data received: ${JSON.stringify(data)}`);
+	};
 </script>
 
-<Button class="w-full" href="/create-review">Check in</Button>
+<div>
+	{#each data.restaurant as item}
+		<p>{item.id}</p>
+		<p>{item.created_at}</p>
+		<p>{item.owner_id}</p>
+		<p>{item.name}</p>
+		<p>{item.description}</p>
+		<p>{item.is_verified}</p>
+		<p>{item.res_images}</p>
+		<p>{item.menu_images}</p>
+		<p>{item.cuisine}</p>
+		<p>{item.address}</p>
+		<p>{item.logo_url}</p>
+	{/each}
+</div>
+
+<main>
+	
+
+	<Button disabled={webSocketEstablished} on:click={() => establishWebSocket()}>
+		Establish WebSocket connection
+	</Button>
+
+	<Button on:click={() => requestData(`test-name`)}> check-in (will send back qr hard-coded for n) </Button>
+
+		<ul>
+			{#each log as event}
+				{#if event.includes("image/png")}
+					<img src="{event.slice(30)}" alt="QR"/>
+				{/if}
+				<li>{event}</li>
+			{/each}
+		</ul>
+	</main>
+
+<style>
+	main {
+		font-family: sans-serif;
+	}
+</style>
--- src/routes/places/[name]/+server.ts
+++ src/routes/places/[name]/+server.ts
@@ -2,22 +2,22 @@ import { json } from '@sveltejs/kit';
 import qrCode from 'qrcode';
 
 export const GET = async ({ locals: { supabase, wss, getSession }, params, url }) => {
-  // const session = await getSession();
-  // if (!session) {
-  //   return json({ status: 500, body: "error cuh" });
-  // }
+  const session = await getSession();
+  if (!session) {
+    return json({ status: 500, body: "error cuh" });
+  }
 
     if (wss) {
       wss.clients.forEach((client: { readyState: number; send: (arg0: string) => void; }) => {
         if (client.readyState === 1) {
-          qrCode.toDataURL(`https://localhost:3004/places/${params.name}/create-review`, (err: any, qrDataURL: string) => {
+          qrCode.toDataURL(`https://localhost:5173/places/${params.name}/create-review`, (err: any, qrDataURL: string) => {
             if (err) {
               client.send('Error generating QR code');
             } else {
               client.send(qrDataURL);
 
             }
-            return qrDataURL
+  
           });
         }
       });
