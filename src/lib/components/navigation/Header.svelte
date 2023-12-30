ceb528d6163f48e9c74485a803d60244f9907d88
Quân Đỗ <aloha.quando@gmail.com>
2023-12-30 18:12:52 +0700

    Added favicon

--- src/lib/components/navigation/Header.svelte
+++ src/lib/components/navigation/Header.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import Logo from '$lib/components/logos/Logo.svelte';
-	import Image from '$lib/components/media/Image.svelte';
 
 	export let href: string = '/';
-	export let userSrc: string;
 
 	export let type: 'default' | 'management' = 'default';
 	let customClasses = '';
@@ -11,11 +9,10 @@
 </script>
 
 <div class="h-14 border-b border-gray-200 flex bg-white w-full {customClasses}">
-	<div class="container mx-auto flex items-center justify-between pr-6">
+	<div class="container mx-auto flex items-center justify-start pr-6">
 		<Logo
 			{href}
 			{type}
 		/>
-		<!--		<Image class="w-8 h-8 rounded-full ring-1 ring-gray-200" alt="Profile image" src={userSrc} />-->
 	</div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,9 @@
 	import { onMount } from 'svelte';
 	import NavBar from '$lib/components/navigation/NavBar.svelte';
 	import Header from '$lib/components/navigation/Header.svelte';
-	import Image from '$lib/components/media/Image.svelte';
-	import bg from '$lib/assets/bg/bg.avif';
-	import Button from '$lib/components/buttons/Button.svelte';
-	import Body from '$lib/components/typography/Body.svelte';
+	import faviconPNG from '$lib/assets/icons/favicons/favicon.png';
+	import faviconSVG from '$lib/assets/icons/favicons/icon.svg';
+	import faviconApple from '$lib/assets/icons/favicons/apple-touch-icon.png';
 
 	export let data;
 
@@ -26,98 +25,32 @@
 
 		return () => subscription.unsubscribe();
 	});
-
-	const publicUrl = supabase.storage
-		.from('avatars')
-		.getPublicUrl(session?.user.user_metadata.avatar_url);
-
-	let title = 'The Red Cafe';
-	let restaurantSrc =
-		'https://bcassetcdn.com/public/blog/wp-content/uploads/2019/07/18094833/the-red-cafe.png';
-	let userSrc =
-		'https://m.media-amazon.com/images/M/MV5BNDM1YjNkZjEtNTllMC00M2M3LTg4OGYtMjYyYTRmMzNjNjRjXkEyXkFqcGdeQXVyNDY5MjMyNTg@._V1_.jpg';
-
-	let debug: boolean = true;
-	let signIn = data.session;
-	let email: string = '';
 </script>
 
 <svelte:head>
 	<title>YumRank</title>
+	<link
+		href={faviconPNG}
+		rel="icon"
+		sizes="32x32"
+	/>
+	<link
+		href={faviconSVG}
+		rel="icon"
+		type="image/svg+xml"
+	/>
+	<link
+		href={faviconApple}
+		rel="apple-touch-icon"
+	/>
 </svelte:head>
 
-<Image
-	alt="Background"
-	class="hidden sm:block fixed top-0 bottom-0 left-0 right-0 -z-50 object-cover ring h-screen w-full"
-	src={bg}
-/>
-{#if debug}
-	<div class="fixed h-screen w-screen top-0 left-0 p-2">
-		<div
-			class="space-y-4 w-fit p-4 rounded-2xl items-end justify-center bg-white flex-col flex ml-auto max-w-lg"
-		>
-			<Image
-				class="w-8 h-8 rounded-full"
-				src={publicUrl.data.publicUrl}
-			/>
-			<Body>Signed {signIn ? 'in as \n' + data.session?.user.user_metadata.user_name : 'out'}</Body>
-			{#if signIn}
-				<Button
-					width="full"
-					design="tonal">Sign out</Button
-				>
-			{:else}
-				<Button
-					width="full"
-					design="tonal">Sign in</Button
-				>
-			{/if}
-		</div>
-	</div>
-{/if}
-<div class="hidden sm:block bg-red-500/50 fixed top-0 bottom-0 left-0 right-0 -z-40" />
-<div class="hidden sm:block bg-white/80 fixed top-0 bottom-0 left-0 right-0 -z-30" />
+<div class="relative mx-auto min-h-screen bg-white pb-20">
+	<Header class="sticky top-0 z-50" />
 
-<div class="sm:w-[40rem] shadow-2xl min-h-screen mx-auto bg-white relative">
-	<Header
-		class="sticky top-0 z-50"
-		userSrc={session?.user.app_metadata.provider === 'email'
-			? publicUrl.data.publicUrl
-			: session?.user.user_metadata.avatar_url}
-	/>
-	<!--	<RestaurantSwitcher {title} {restaurantSrc} />-->
-	<!--	<TabBarManagement />-->
-
-	<div class=" mx-auto px-6 bg-white">
+	<div class="container mx-auto bg-white px-6 sm:max-w-[48rem]">
 		<slot />
 	</div>
 
-	<!-- Spacing to avoid bottom nav bar-->
-	<div class="h-20 bg-white" />
-	<NavBar class="fixed bottom-0 w-full sm:w-[40rem]" />
+	<NavBar class="fixed bottom-0 left-0 w-full" />
 </div>
-
-<!--<nav style="border: solid; border-width: 0 0 2px; padding-bottom: 5px;">-->
-<!--	<a href="/">Home</a>-->
-<!--	{#if session}-->
-<!--		<a href="/app">App</a>-->
-<!--		<a href="/admin">Admin</a>-->
-<!--		{#if session.user.app_metadata.provider == 'email'}-->
-<!--			<img-->
-<!--				style="width: 32px; height: 32px; border-radius: 9999px;"-->
-<!--				src={publicUrl.data.publicUrl}-->
-<!--				alt="person_avatar"-->
-<!--			/>-->
-
-<!--		{:else}-->
-<!--			<img-->
-<!--				style="width: 32px; height: 32px; border-radius: 9999px;"-->
-<!--				src={session.user.user_metadata.avatar_url}-->
-<!--				alt="person_avatar"-->
-<!--			/>-->
-<!--		{/if}-->
-
-<!--	{:else}-->
-<!--		<a href="/auth">Login</a>-->
-<!--	{/if}-->
-<!--</nav>-->
