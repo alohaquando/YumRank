e50a3b567d17648e241b6393d2e69f5c01ff712d
Quân Đỗ <aloha.quando@gmail.com>
2024-01-08 12:15:50 +0700

    Added my managed places

---
+++ src/routes/me/managed-places/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	// noinspection ES6UnusedImports
+	import Fa from 'svelte-fa';
+	import LargePageTitle from '$lib/components/layouts/LargePageTitle.svelte';
+	import LeaderboardItem from '$lib/components/leaderboard/LeaderboardItem.svelte';
+	import {
+		placeSrcExample,
+		dishSrcExample,
+		placeNameExample,
+		dishNameExample,
+		checkInsExample,
+		ratingExample,
+		favoritesExample,
+		hrefExample
+	} from '$lib/data/exampleData';
+	import Button from '$lib/components/buttons/Button.svelte';
+	import { faPlus } from '@fortawesome/pro-regular-svg-icons';
+
+	export let data;
+</script>
+
+<LargePageTitle showBackButton>Managed places</LargePageTitle>
+
+<div class="flex-col space-y-10 pb-6">
+	{#if data.myPlaces}
+		{#each data.myPlaces as place, i}
+			<!--	TODO: Get actual ranking AND favorite count -->
+			<LeaderboardItem
+				rank={i + 1}
+				restaurantSrc={place.res_images != null ? place.res_images[0] : ''}
+				restaurantName={place.name}
+				checkIns={place.numReviews}
+				rating={place.avgRating}
+				favorites={$favoritesExample}
+				href="/places/{place.name}"
+			/>
+		{/each}
+	{/if}
+</div>
+
+<div class="bg-white sticky bottom-16 py-4 border-t">
+	<Button
+		href="/add-place"
+		width="full"
+	>
+		<Fa
+			icon={faPlus}
+			slot="icon"
+		/>
+		Add a place
+	</Button>
+</div>
